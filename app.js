5802a58ef05fec21df589c45d340e0a3ab506c83
abdullohn001 <muhammadaligulomov41@gmail.com>
2024-10-13 10:38:47 +0500

    add keyboard mode

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
-document.addEventListener("DOMContentLoaded", function () {
+function music() {
   const buttons = document.querySelectorAll(".drum");
 
   function playSound(soundFile) {
     const audio = new Audio(soundFile);
     audio.play();
   }
+
+  document.addEventListener("keypress", (e) => {
+    switch (e.key) {
+      case "w":
+        playSound("sounds/crash.mp3");
+        break;
+      case "a":
+        playSound("sounds/kick-bass.mp3");
+        break;
+      case "s":
+        playSound("sounds/snare.mp3");
+        break;
+      case "d":
+        playSound("sounds/tom-1.mp3");
+        break;
+      case "j":
+        playSound("sounds/tom-2.mp3");
+        break;
+      case "k":
+        playSound("sounds/tom-3.mp3");
+        break;
+      case "l":
+        playSound("sounds/tom-4.mp3");
+        break;
+      default:
+    }
+  });
+
   buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
       const target = e.target;
@@ -36,5 +64,6 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
-});
+}
 
+music();
